a31d752a43f7b0f3879452050a7d02f46c87a894
Shariful4447 <shariful.islam4447@gmail.com>
2023-09-15 02:08:54 +0200

    setup done project started

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <div className='container'>
+            <h1>hello world</h1>
+            
+        </div>
+    );
+};
+
+export default Home;
